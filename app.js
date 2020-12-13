8d0c32076892002df3cdd8829cf9f471f4ac8938
rkjain119 <rkjain119@gmail.com>
2020-12-13 23:36:54 +0530

    Updated style.css && app.js

---
+++ app.js
@@ -0,0 +1,24 @@
+var inputTranslateText = document.querySelector("#input-txt");
+var outputTranslateText = document.querySelector("#output-txt");
+
+var  btnTranslator = document.querySelector("#btn-tlate");
+btnTranslator.addEventListener("click", btnClick);
+
+var server = "https://api.funtranslations.com/translate/minion.json";
+
+function btnClick(e) {
+    var input = inputTranslateText.value;
+    var finalURL = constructURL(input);
+
+    fetch(finalURL)
+        .then(response => response.json())
+        .then(json => {
+            outputTranslateText.innerText = json.contents.translated;
+        })
+        .catch(() => alert("Server Error Please Try After Sometime"))
+}
+
+function constructURL(inputText) {
+    var encodedURI = encodeURI(inputText);
+    return "${server}? text=${encodedURI}";
+}
